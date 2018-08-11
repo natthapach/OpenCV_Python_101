cbe5d251cad47edf22a27f18fe0792295a03c3f6
natthapach <natthapach.a@ku.th>
2018-08-11 11:04:48 +0800

    try open webcam and show

---
+++ webcam_read.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+CAMERA_CHANELL = 0  #index of cemera in computer
+cap = cv2.VideoCapture(CAMERA_CHANELL)
+
+WINDOW_TITLE_1 = 'Webcam Color'
+WINDOW_TITLE_2 = 'Webcam Gray'
+
+while True:
+  ret, frame = cap.read()
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  cv2.imshow(WINDOW_TITLE_1, frame)
+  cv2.imshow(WINDOW_TITLE_2, gray)
+
+
+  key = cv2.waitKey(1)
+  if key == ord('q'):
+    break
+
+cap.release()
+cv2.destroyAllWindows()
